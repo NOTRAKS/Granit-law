cec896bd5afaf1fbb153865b3104f4ed91f98a91
ГОРДЕЙ <skvorzovgordei@gmail.com>
2026-01-12 11:37:02 +0300

    Configure static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Granit-law',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 export default nextConfig
